88f0ce31338497ce67872dffd9d02bb90144108c
feed0 <dogmas-gutters.07@icloud.com>
2026-02-20 07:37:36 -0300

    Initial Commit

---
+++ GroceryList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryList
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 20/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
